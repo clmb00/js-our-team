4485e7a14b8bc311286db5febe1e32d30d6d62fd
clmb00 <paolo.colombo2000@gmail.com>
2022-10-25 15:34:52 +0200

    [add] milestone 0

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const teamMembers = [
+  {
+    name: 'Wayne Barnet',
+    role: 'Founder & CEO',
+    photo: 'wayne-bernett-founder-ceo.jpg'
+  }, {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    photo: 'angela-caroll-chief-editor.jpg'
+  }, {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    photo: 'walter-gordon-office-manager.jpg'
+  }, {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jpg'
+  }, {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    photo: 'scott-estrada-developer.jpg'
+  }, {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jpg'
+  }
+]
